bc85a178df1685241f850ebede85a2aa581d8058
Danilomd <danilomdaim@gmail.com>
2025-06-09 23:12:33 -0300

    creating orders and invoices services

---
+++ app-orders/src/http/server.ts
@@ -0,0 +1,62 @@
+import { fastify } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { fastifyCors } from '@fastify/cors'
+import { custom, z } from 'zod'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { channels } from '../broker/channels/index.ts'
+import { db } from '../db/client.ts'
+import { schema } from '../db/schema/index.ts'
+import { dispatchOrderCreated } from '../broker/messages/order-created.ts'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, { origin: '*'})
+
+
+
+app.get('/health', () => {
+  console.log('Health check'); // Agora este será executado
+  return 'OkkkkK';
+})
+
+app.post('/orders', {
+  schema: {
+    body: z.object({
+      amount: z.coerce.number(),
+    })
+  }
+}, async (request, reply) => {
+  const { amount } = request.body
+
+  console.log('Received order with amount:', amount)
+  //channels.orders.sendToQueue('orders', Buffer.from('Hello World!'))
+
+  const orderId = randomUUID()
+  dispatchOrderCreated({
+    orderId,
+    amount,
+    customer:{
+      id: 'asdasdasdads',
+    }
+  })
+
+  await db.insert(schema.orders).values({
+    id: randomUUID(),
+    customerId: 'asdasdasdads',
+    amount,
+  })
+  return reply.status(201).send()
+})
+
+
+app.listen({ host: '0.0.0.0', port: 3333}).then (() => {
+  console.log('[Orders] is running')
+})
