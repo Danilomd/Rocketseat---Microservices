04789e35e3d1e05eb46e0ef6a7deb0969372c61d
Danilomd <danilomdaim@gmail.com>
2025-06-11 22:04:23 -0300

    Adding trace and timeout to orders

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -1,33 +1,31 @@
 import '@opentelemetry/auto-instrumentations-node/register'
-import { setTimeout } from 'node:timers/promises'
+
 import { fastify } from 'fastify'
 import { randomUUID } from 'node:crypto'
+import { setTimeout } from 'node:timers/promises'
 import { fastifyCors } from '@fastify/cors'
 import { trace } from '@opentelemetry/api'
-import { custom, z } from 'zod'
+import { z } from 'zod'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { channels } from '../broker/channels/index.ts'
-import { db } from '../db/client.ts'
 import { schema } from '../db/schema/index.ts'
+import { db } from '../db/client.ts'
 import { dispatchOrderCreated } from '../broker/messages/order-created.ts'
+import { tracer } from '../tracer/tracer.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
-
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
-app.register(fastifyCors, { origin: '*'})
-
-
+app.register(fastifyCors, { origin: '*' })
 
 app.get('/health', () => {
-  console.log('Health check'); // Agora este será executado
-  return 'OkkkkK';
+  return 'OK'
 })
 
 app.post('/orders', {
@@ -39,34 +37,37 @@ app.post('/orders', {
 }, async (request, reply) => {
   const { amount } = request.body
 
-  console.log('Received order with amount:', amount)
-  //channels.orders.sendToQueue('orders', Buffer.from('Hello World!'))
+  console.log('Creating an order with amount', amount)
 
   const orderId = randomUUID()
 
-
   await db.insert(schema.orders).values({
-    id: randomUUID(),
-    customerId: 'asdasdasdads',
+    id: orderId,
+    customerId: 'B9176D35-7276-4255-A323-D825CAEE03B5',
     amount,
   })
 
-  //await setTimeout(2000)
-  //trace.getActiveSpan()?.setAttribute('order_id', orderId)
+  const span = tracer.startSpan('eu acho que aqui ta dando merda')
+
+  span.setAttribute('teste', 'Hello World')
+
+  await setTimeout(2000)
+
+  span.end()
+
+  trace.getActiveSpan()?.setAttribute('order_id', orderId)
 
   dispatchOrderCreated({
     orderId,
     amount,
-    customer:{
-      id: 'asdasdasdads',
-    }
+    customer: {
+      id: 'B9176D35-7276-4255-A323-D825CAEE03B5',
+    },    
   })
-
 
   return reply.status(201).send()
 })
 
-
-app.listen({ host: '0.0.0.0', port: 3333}).then (() => {
-  console.log('[Orders] is running')
+app.listen({ host: '0.0.0.0', port: 3333 }).then(() => {
+  console.log('[Orders] HTTP Server running!')
 })
